ffd8aee54b930af71dbbc6803fa8c4638c5c214f
Deivy Spaulonci <42875800+deivy-spaulonci@users.noreply.github.com>
2024-04-29 15:58:57 -0300

    inicio do projeto

---
+++ src/main/java/com/br/apptecnico/AppTecnicoApplication.java
@@ -0,0 +1,13 @@
+package com.br.apptecnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTecnicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTecnicoApplication.class, args);
+	}
+
+}
